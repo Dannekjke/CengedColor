aaf81e96289ef90b753c38b29281b93256356e69
Dannekjke <89574332+Dannekjke@users.noreply.github.com>
2022-02-16 11:05:01 +0300

    asd

--- CengedColor/ViewController.swift
+++ CengedColor/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLabel.text = String(redSlider.value)
+        greenLabel.text = String(greenSlider.value)
+        blueLabel.text = String(blueSlider.value)
+        
+        colorView.layer.cornerRadius = 15
     }
 
-
+    @IBAction func rgbSlidersChanges(_ sender: UISlider) {
+        changedColor()
+        switch sender {
+        case redSlider:
+            redLabel.text = String(redSlider.value)
+        case greenSlider:
+            greenLabel.text = String(greenSlider.value)
+        default:
+            blueLabel.text = String(blueSlider.value)
+        }
+    }
+    func changedColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1)
+    }
+   
+    
 }
 
